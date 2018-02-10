3d1b1ea5c712e16f84e7d8f3f3741ef0c534f2e2
liemgearen15 <liemgearen15@augustana.edu>
2018-02-10 13:11:55 -0600

    Somehow added working backtracking with a state machine idea. Some good stuff in this. Worked with Blake on this.

--- src/SimMazeSolver.java
+++ src/SimMazeSolver.java
@@ -1,17 +1,23 @@
+import java.awt.Point;
 import java.io.FileNotFoundException;
-import java.util.PriorityQueue;
-import java.util.Stack;
-
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import com.stonedahl.robotmaze.SimRobot;
 
 public class SimMazeSolver {
-
+	
+static char direction = '^';
+static Point coordinates = new Point();
+static int state = 0; //0 is searching, 1 is backtracking
+static ArrayList<Point> map = new ArrayList<Point>();
 	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-		Stack movesMade =new Stack();
-		PriorityQueue reversedActions = new PriorityQueue();
+		Stack<Character> movesMade =new Stack<Character>();
+		coordinates.x = 0;
+		coordinates.y = 0;//home
+		map.add(coordinates);
+		ArrayBlockingQueue<Character> reversedActions;
 		SimRobot simRobot = new SimRobot("maze1.txt", 100); // 500 ms animation
 																// delay...
-		
 		simRobot.neckRight90();
 		float distRight = simRobot.getDistanceMeasurement();
 		simRobot.neckLeft90();
@@ -21,7 +27,6 @@ public class SimMazeSolver {
 		simRobot.neckRight90();
 		Thread.sleep(2000);
 		int moves = 1;
-
 		// current pathfinding, just goes towards the most white space it sees
 		while (simRobot.colorSensorSeesGoal() != true) {
 			float oldDistRight = distRight;
@@ -35,114 +40,270 @@ public class SimMazeSolver {
 				distLeft = simRobot.getDistanceMeasurement();
 				System.out.print("Distances Sensed:  R: " + distRight + " S: " + distStraight + " L:" + distLeft);
 				simRobot.neckRight90();
-				Thread.sleep(2000);
+				System.out.println(isVisited());
 			}
-			if (distLeft < distRight && distRight > oldDistRight) {
-				System.out.println("Turn right");
-				simRobot.right90();
-				simRobot.forwardOneCell();
-				movesMade.push('>');
-			} else if (distLeft > distRight && distLeft > oldDistLeft) {
-				System.out.println("Turn left.");
-				simRobot.left90();
-				simRobot.forwardOneCell();
-				movesMade.push('<');
-			} else if (distStraight > 1) {
-				System.out.println("We can go straight");
-				// Note: the move should always succeed, because we checked for
-				// walls ahead first and
-				// the simulator distance sensor is always accurate (unlike the
-				// physical sensor!)
-				simRobot.forwardOneCell();
-				movesMade.push('^');
-				// if moveSuceeded were false, that would mean it hit a wall
-				// (and the robot's bump sensor was activated)
+				if (isVisited() && state == 0){
+					if (distRight > distStraight) {
+						simRobot.right90();
+						changeDirection('>');
+						movesMade.push('>');
+					} else if (distLeft > distStraight) {
+						simRobot.left90();
+						changeDirection('<');
+						movesMade.push('<');
+					} 
+					else if (distLeft == distRight) {
+//						simRobot.right90();
+//						changeDirection('>');
+//						simRobot.right90();
+//						changeDirection('>');
+//						movesMade.push('>');
+//						movesMade.push('>');
+						state = 1;
+					}
+				} else if (state == 1){
+					if (distStraight > 1) {
+						System.out.println("We can go straight");
+						// Note: the move should always succeed, because we checked for
+						// walls ahead first and
+						// the simulator distance sensor is always accurate (unlike the
+						// physical sensor!)
+						simRobot.forwardOneCell();
+						changeCoord();
+						state=0;
+						Point temp = new Point();
+						temp.x = coordinates.x;
+						temp.y = coordinates.y;
+						//map.set(map.size() -1, temp);
+						map.add(map.size()-1, temp);
+						movesMade.push('^');
+						// if moveSuceeded were false, that would mean it hit a wall
+						// (and the robot's bump sensor was activated)
 
-				if (simRobot.colorSensorSeesGoal()) {
-					System.out.println("FOUND GOAL!");
-					System.out.println("Now, if only I could find my way home...");
-					movesMade.push('G');
-//					retrace(reversedActions, simRobot);
-					break; // break out of the FOR loop early
+					} else if (distRight > distStraight) {
+						simRobot.right90();
+						changeDirection('>');
+						movesMade.push('>');
+					} else if (distLeft > distStraight) {
+						simRobot.left90();
+						changeDirection('<');
+						movesMade.push('<');
+					} else if (distLeft == distRight) {
+						simRobot.right90();
+						changeDirection('>');
+						simRobot.right90();
+						changeDirection('>');
+						movesMade.push('>');
+						movesMade.push('>');
+					}
+				}else{
+				Thread.sleep(2000);
+				if (distLeft < distRight && distRight > oldDistRight) {
+					System.out.println("Turn right");
+					simRobot.right90();
+					changeDirection('>');
+					simRobot.forwardOneCell();
+					changeCoord();
+					state = 0;
+					Point temp = new Point();
+					temp.x = coordinates.x;
+					temp.y = coordinates.y;
+					//map.set(map.size() -1, temp);
+					map.add(map.size()-1, temp);
+					movesMade.push('>');
+					movesMade.push('^');
+					state = 0;
+				} else if (distLeft > distRight && distLeft > oldDistLeft) {
+					System.out.println("Turn left.");
+					simRobot.left90();
+					changeDirection('<');
+					simRobot.forwardOneCell();
+					changeCoord();
+					state=0;
+					Point temp = new Point();
+					temp.x = coordinates.x;
+					temp.y = coordinates.y;
+					//map.set(map.size() -1, temp);
+					map.add(map.size()-1, temp);
+					movesMade.push('<');
+					movesMade.push('^');
+					state = 0;
+				} else if (distStraight > 1) {
+					System.out.println("We can go straight");
+					// Note: the move should always succeed, because we checked for
+					// walls ahead first and
+					// the simulator distance sensor is always accurate (unlike the
+					// physical sensor!)
+					simRobot.forwardOneCell();
+					changeCoord();
+					state=0;
+					Point temp = new Point();
+					temp.x = coordinates.x;
+					temp.y = coordinates.y;
+					//map.set(map.size() -1, temp);
+					map.add(map.size()-1, temp);
+					movesMade.push('^');
+					// if moveSuceeded were false, that would mean it hit a wall
+					// (and the robot's bump sensor was activated)
+	
+					if (simRobot.colorSensorSeesGoal()) {
+						System.out.println("FOUND GOAL!");
+						System.out.println("Now, if only I could find my way home...");
+						movesMade.push('G');
+						
+						break; // break out of the FOR loop early
+					}
+				} else if (distRight > distStraight) {
+					simRobot.right90();
+					changeDirection('>');
+					movesMade.push('>');
+				} else if (distLeft > distStraight) {
+					simRobot.left90();
+					changeDirection('<');
+					movesMade.push('<');
+				} else if (distLeft == distRight) {
+					simRobot.right90();
+					changeDirection('>');
+					simRobot.right90();
+					changeDirection('>');
+					movesMade.push('>');
+					movesMade.push('>');
+					state = 1;
 				}
-			} else if (distRight > distStraight) {
-				simRobot.right90();
-				movesMade.push('>');
-			} else if (distLeft > distStraight) {
-				simRobot.left90();
-				movesMade.push('<');
-			} else if (distLeft == distRight) {
-				simRobot.right90();
-				simRobot.right90();
 			}
 			moves++;
-			}
-		System.out.println("Got to the goal " + reversedActions.isEmpty());
+		}
+		simRobot.right90();
+		changeDirection('>');
+		simRobot.right90();
+		changeDirection('>');
+		//System.out.println("Got to the goal " + reversedActions.isEmpty());
 		System.out.println("Moves made: " + movesMade);
+		reversedActions = new ArrayBlockingQueue<Character>(movesMade.size());
 		reversedActions = reverseActions(movesMade);
-		while(!(reversedActions.isEmpty())){
+		System.out.println(coordinates.x + " , " + coordinates.y);
+		while(coordinates.x!=0 || coordinates.y!=0){
 			char temp = (char) reversedActions.remove();
+			simRobot.neckRight90();
+			distRight = simRobot.getDistanceMeasurement();
+			simRobot.neckLeft90();
+			distStraight = simRobot.getDistanceMeasurement();
+			simRobot.neckLeft90();
+			distLeft = simRobot.getDistanceMeasurement();
+			simRobot.neckRight90();
 			System.out.println("Next move: " + temp);
 			if (temp=='<'){
 				simRobot.left90();
-				simRobot.forwardOneCell();
-				
+				changeDirection('<');
+				if (distLeft > 1){
+					simRobot.forwardOneCell();
+					changeCoord();
+					Point temp1 = new Point();
+					temp1.x = coordinates.x;
+					temp1.y = coordinates.y;
+					//map.set(map.size() -1, temp1);
+					map.add(map.size()-1, temp1);
+				}
 			}
 			else if (temp=='>'){
 				simRobot.right90();
-				simRobot.forwardOneCell();
+				changeDirection('>');
+				if (distRight > 1){
+					simRobot.forwardOneCell();
+					changeCoord();
+					state = 0;
+					Point temp1 = new Point();
+					temp1.x = coordinates.x;
+					temp1.y = coordinates.y;
+					//map.set(map.size() -1, temp1);
+					map.add(map.size()-1, temp1);
+				}
 			
 			}
 			else if (temp == '^'){
-				simRobot.forwardOneCell();
-			} else {//temp == 'G'
-				simRobot.right90();
-				simRobot.right90();
+				if (distStraight > 1){
+					simRobot.forwardOneCell();
+					changeCoord();
+					state = 0;
+					Point temp1 = new Point();
+					temp1.x = coordinates.x;
+					temp1.y = coordinates.y;
+					//map.set(map.size() -1, temp1);
+					map.add(map.size()-1, temp1);
+				}
 			}
 		}
-
-			/*
-			 * Pseudocode from the internet that may be useful while(myPos !=
-			 * goal) //Scan left, forward, right to determine if theirs a
-			 * path(edge) to an adjacent node(vertex) //create new node object
-			 * and add to FIFO stack //if no new paths backtrack to deepest node
-			 * on stack
-			 * 
-			 * //scan right and compare to a pre defined distance to a wall if
-			 * (scan right > wall distance && direction = north) { CREATE new
-			 * node object (X co-ordinate +1 + Y co-ordinate + east) add to
-			 * stack }
-			 * 
-			 * elseif (scan right > wall distance && direction = east) { CREATE
-			 * new node object (X co-ordinate + Y co-ordinate -1 + south) add to
-			 * stack }
-			 * 
-			 * etc etc
-			 * 
-			 * scan front…
-			 * 
-			 * scan left…
-			 * 
-			 * Next move pop node object off stack
-			 * 
-			 * direction of travel = new direction in node object call
-			 * appropriate movement methods
-			 * 
-			 */
+	}
+	public static boolean isVisited(){
+		boolean visited = false;
+		Point temp = new Point(coordinates.x, coordinates.y);
+		if (direction == '^'){
+			temp.y++;
+		} else if (direction == '>'){
+			temp.x++;
+		} else if (direction == 'v'){
+			temp.y--;
+		}else{
+			temp.x--;
 		}
+		if (map.contains(temp)){
+			visited = true;
+		}	
+//		for (Point element : map){
+//			System.out.println(element.x +  ", " + element.y);
+//		}
+		return visited;
+	}
+	public static void changeDirection(char turn){
+		if(turn == '<'){
+			if(direction == '^'){
+				direction = '<';
+			}
+			else if(direction == '>'){
+				direction = '^';
+			}
+			else if(direction == 'v'){
+				direction = '>';
+			}else{//direction is '<'
+				direction = 'v';
+			}
+		}
+		else{
+			if(direction == '^'){
+				direction = '>';
+			}
+			else if(direction == '>'){
+				direction = 'v';
+			}
+			else if(direction == 'v'){
+				direction = '<';
+			}else{//direction is '<'
+				direction = '^';
+			}
+		}
+	}
+	public static void changeCoord(){
+		if(direction == '^'){
+			coordinates.y++;
+		}else if (direction == '>'){
+			coordinates.x++;
+		}else if (direction == 'v'){
+			coordinates.y--;
+		}else{//direction is '<'
+			coordinates.x--;
+		}
+	}
+
 	/**
 	 * reverses the order of the previous actions so the robot can find its way home
 	 */
-	public static PriorityQueue reverseActions(Stack movesMade){
+	public static ArrayBlockingQueue<Character> reverseActions(Stack<Character> movesMade){
 		System.out.println("called reverseActions function");
-		PriorityQueue reversedActions = new PriorityQueue();
+		ArrayBlockingQueue<Character> reversedActions = new ArrayBlockingQueue<Character>(movesMade.size());
 		//TODO: do something to get rid of unnecessary moves
 		while(!movesMade.isEmpty()){
-			System.out.println("Making the reversed actions");
 			char temp = (char) movesMade.pop();
-			if(temp=='G'){//do nothing
-			}
-			else if (temp=='<'){
+			if (temp=='<'){
 				reversedActions.add('>');
 			}
 			else if (temp=='>'){
@@ -155,92 +316,6 @@ public class SimMazeSolver {
 		System.out.println("reversedActions: " + reversedActions);
 		return reversedActions;
 	}
-	public static void retrace(Stack reversedActions, SimRobot simRobot){
-		while(!reversedActions.isEmpty()){
-			char temp = (char) reversedActions.pop();
-			if (temp=='<'){
-				simRobot.left90();
-				simRobot.forwardOneCell();
-				break;
-			}
-			else if (temp=='>'){
-				simRobot.right90();
-				simRobot.forwardOneCell();
-				break;
-			}
-			else{// (temp=='^'){
-				simRobot.forwardOneCell();
-				break;
-			}
-		}
-	}
-		//Backtracking method pseudo found online
-		// Get the start location (x,y) and try to solve the maze
-		/*public static int counter = 0;
-		public void solve(int x, int y) {
-			if (step(x,y)) {
-				maze[x][y] = 'S';
-				
-			}
-		}
-		
-		// Backtracking method
-		public boolean step (int x, int y) {
-			
-			counter++;			
-			//** Accept case - we found the exit **
-			if (maze[x][y] == 'G') {
-				return true;
-			}
-			
-			//** Reject case - we hit a wall or our path **
-			if (maze[x][y] == 'X' || maze[x][y] == '.') {
-				return false;
-			}
-			
-			/** Backtracking Step *
-			
-			// Mark this location as part of our path
-			maze[x][y] = '.';
-			boolean result;	
-			
-			// Try to go Right
-			result = step(x, y+1);
-			if (result) { return true;}
-			
-			// Try to go Up
-			result = step(x-1, y);
-			if (result) { return true;}
-			
-			// Try to go Left
-			result = step(x, y-1);
-			if (result) { return true;}		
-			
-			// Try to go Down
-			result = step(x+1, y);
-			if (result) { return true;}		
-			
-			
-			/** Deadend - this location can't be part of the solution *
-			
-			// Unmark this location
-			maze[x][y] = ' ';
-			
-			// Go back
-			return false;
-		}
-		
-		public String toString() {
-			String output = "";
-			for (int x=0; x<10; x++) {
-				for (int y=0; y<10; y++) {
-					output += maze[x][y] + " ";
-				}
-				output += "\n";
-			}
-			return output;
-		}*/
-	
-	}
+}
 
 
